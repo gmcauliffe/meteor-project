ae7938d502855e058363ed09a68f50f252471637
Guy McAuliffe <guy.mcauliffe@icloud.com>
2018-11-18 15:08:18 -0800

    Remember me feature added to Login page

--- imports/ui/pages/Login_page.js
+++ imports/ui/pages/Login_page.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import RememberMe from 'meteor/tprzytula:remember-me';
 
 import './Login_page.html';
 
@@ -35,15 +36,17 @@ if (Meteor.isClient) {
       submitHandler: function submitHandler(event) {
         const username = $('[name=loginUsername]').val();
         const password = $('[name=loginPassword]').val();
-        Meteor.loginWithPassword(username, password, (error) => {
+        const rememberMe = $('[name=loginRememberMe]').prop('checked');
+        RememberMe.loginWithPassword(username, password, (error) => {
           if (error) {
             validator.showErrors({
               loginUsername: error.reason,
             });
           } else {
+            console.log("RememberMe", rememberMe);
             FlowRouter.go('/');
           }
-        });
+        }, rememberMe);
       },
     });
   });
