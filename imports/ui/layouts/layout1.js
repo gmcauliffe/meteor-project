03c9c2f2adb0973c1caa99fe8ad9699e08c3a7eb
Guy McAuliffe <guy.mcauliffe@icloud.com>
2018-11-19 14:31:18 -0800

    Second layout added for non-login/registration pages

---
+++ imports/ui/layouts/layout1.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating';
+
+import './layout1.html';
+import '../pages/Coming_page';
+import '../pages/Home_page';
+import '../components/App_forcedLoggedIn.html';
+
+if (Meteor.isClient) {
+  Template.layout1.onCreated(() => {
+    console.log("The 'layout1' template was just created.");
+  });
+  Template.layout1.onRendered(() => {
+    document.body.style.backgroundImage = "url('/img/photos/photo6@2x.jpg')";
+  });
+  Template.layout1.onDestroyed(() => {
+    document.body.style.backgroundImage = 'none';
+    document.body.style.backgroundColor = 'white';
+  });
+}
